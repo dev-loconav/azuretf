ebb670e7a1dd79b26e211920ab25a8bfca0033ab
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-05-16 16:41:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,3 +23,32 @@ variable "tenant_id" {
     description =   "Tenant ID"
     type        =   string
 }
+variable "resource_group_name" {
+    description =   "Name of the resource group"
+    type        =   string
+
+}
+
+variable "location" {
+    description =   "Location of the resource group"
+    type        =   string
+    default     =   "East US"
+}
+
+variable "virtual_network_name" {
+    description =   "Name of the virtual network"
+    type        =   string
+
+}
+
+variable "vnet_address_range" {
+    description =   "IP Range of the virtual network"
+    type        =   string
+
+}
+
+variable "subnet" {
+    description     =       "Create multiple subnets"
+    type            =       map(string)
+
+}
