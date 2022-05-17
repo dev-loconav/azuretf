eb8cc1c64e31c60cef3ac2d7b8ebe374cd351b59
dev-loconav <99465109+dev-loconav@users.noreply.github.com>
2022-05-17 09:53:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 # Provider Block
 
 provider "azurerm" {
-    version         =   "=3.0.0"
+    version         =   "3.6.0"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
